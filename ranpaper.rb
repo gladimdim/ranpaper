7c9ccdbe1ab8a67517085079b4b038a565311c42
Dmitry Gladkiy <gladimdim@gmail.com>
2010-10-25 21:25:37 +0300

    Fixed issue with incorrect if - then flow. Main function did not correctly addressed to random_image class variable.

--- ranpaper.rb
+++ ranpaper.rb
@@ -3,14 +3,19 @@
 class Wallpaper
 	attr_reader :result
 	attr_reader :random_image
-	@random_image = nil
-	@wallpaper = ""
-	@result = nil
-	@list_of_images = Array.new 
+	@@random_image = nil 
+	@@wallpaper_dir = nil
+	@@result = nil
+	@@list_of_images = Array.new 
+	def random_image
+		return @@random_image
+	end
 	def initialize(wallpaper_dir)
 		if File::directory?(wallpaper_dir) then
-			@wallpaper_dir = wallpaper_dir
-			result = true
+			@@wallpaper_dir = wallpaper_dir
+			self.get_list_of_images()
+			self.select_random_image()
+			puts @@random_image
 		else
 			result = nil
 		end
@@ -19,23 +24,24 @@ class Wallpaper
 
 	#First we load all files from input directory
 	def get_list_of_files
-		@list_of_files = Dir.entries(@wallpaper_dir) 
-		return @list_of_files
+		@@list_of_files = Dir.entries(@@wallpaper_dir) 
+		return @@list_of_files
 	end
 
 	#Only images with specified extensions "bmp", "jpg", "gif" are left
 	def get_list_of_images
 		if self.get_list_of_files() != nil then 
-			self.get_list_of_files().each { |file| @list_of_images << file if file.to_s.include?("gif") or file.to_s.include?("jpg") or file.to_s.include?("bmp")	}
-		return @list_of_images
+			self.get_list_of_files().each { |file| @@list_of_images << file if file.to_s.include?("gif") or file.to_s.include?("jpg") or file.to_s.include?("bmp")	}
+		return @@list_of_images
 		else return nil
 		end
 	end
 	
 	def select_random_image
-		if @list_of_images != nil then
-			random_image = @list_of_images[0 + rand(@list_of_images.size())]
-			return random_image
+		if @@list_of_images != nil then
+			@@random_image = @@list_of_images[0 + rand(@@list_of_images.size())]
+			puts @@random_image
+			return @@random_image
 		else 
 			return nil
 		end
@@ -43,38 +49,38 @@ class Wallpaper
 end
 
 #Setting path to ~/.ranpaper file which contains single string to wallpapers directory
-@config_file = ENV['HOME'] + "/.ranpaper"
+config_file = ENV['HOME'] + "/.ranpaper"
 
-if !File::file?(@config_file) then
+if !File::file?(config_file) then
 	puts "Please create file in your home directory \"~/.ranpaper\" and add full path to directory with wallpapers. Then rerun program."
 	Kernel.exit
 end
 
-file = File.open(@config_file)
+file = File.open(config_file)
 
-@input_dir = File.read(file).to_s.chomp! 
+input_dir = File.read(file).to_s.chomp! 
 
-if !File::directory?(@input_dir) then
-	puts "Specified directory \""<< @input_dir << "\" could not be found"
+if !File::directory?(input_dir) then
+	puts "Specified directory \""<< input_dir << "\" could not be found"
 	Kernel.exit()
 end
 
-wallpap = Wallpaper.new(@input_dir)
-
+wallpap = Wallpaper.new(input_dir)
+puts wallpap.random_image
 if wallpap.random_image != nil then
-	random_image = @input_dir << wallpap.random_image
+	random_image = input_dir << wallpap.random_image
 	else 
-		puts ("No images where found in " + @input_dir + ". Terminating.")
+		puts ("No images where found in " + input_dir + ". Terminating.")
 		Kernel.exit
 end
 
-@feh_command = "feh --bg-scale " + random_image
-@fbsetbg_command = "fbsetbg -f " + random_image
+feh_command = "feh --bg-scale " + random_image
+fbsetbg_command = "fbsetbg -f " + random_image
 
-value = system(@feh_command)
+value = system(feh_command)
 
 if $?.exitstatus != 0 then
-	value = system(@fbsetbg_command)
+	value = system(fbsetbg_command)
 	if $?.exitstatus == 0 then
 		puts "Wallpaper was set: " + random_image if $?.exitstatus == 0
 		else puts "Error seting wallpaper image: " + random_image
