b25d9e0842c88895377a1e23844e46d794d73e67
Dmitry Gladkiy <gladimdim@gmail.com>
2010-11-11 23:37:10 +0200

    Cleared class variables. Methods moved to public

--- Wallpaper.rb
+++ Wallpaper.rb
@@ -1,24 +1,15 @@
 class Wallpaper
-	attr_reader :result
-	attr_reader :random_image, :list_of_images
-	@random_image = nil 
-	@wallpaper_dir = nil
+	attr_reader :random_image
 	def initialize(wallpaper_dir)
 		if File::directory?(wallpaper_dir) then
 			@wallpaper_dir = wallpaper_dir
 			@list_of_images = Array.new
-			get_list_of_files()
-			get_list_of_images()
-			select_random_image()
 		end
-	
 	end
-
 	#First we load all files from input directory
 	def get_list_of_files
 		@list_of_files = Dir.entries(@wallpaper_dir) 
 	end
-
 	#Only images with specified extensions "bmp", "jpg", "gif" are left
 	def get_list_of_images
 		if @list_of_files != nil then 
@@ -26,9 +17,7 @@ class Wallpaper
 		else
 			@list_of_images = nil
 		end
-	
 	end
-	
 	def select_random_image
 		if @list_of_images != nil and @list_of_images.count != 0 then
 			@random_image = @wallpaper_dir + @list_of_images[0 + rand(@list_of_images.size())]
